b769914a00bf21ec2ecbd070797e0a691f7000e7
edmarkou <edvardas.markevicius@gmail.com>
2018-10-04 18:37:22 +0300

    Fixed the input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import { CSSTransition } from 'react-transition-group'
 import SignInForm from "./SignInForm/SignInForm";
-import lock from './assets/lock.svg';
-import lockAqua from './assets/lockAqua.svg';
-import lockClicked from './assets/lockClicked.svg';
 import './App.css';
 
 class App extends Component {
@@ -11,7 +8,6 @@ class App extends Component {
     super(props);
     this.state = {
       loaded: false,
-      lock: lock
     }
   }
   componentDidMount(){
@@ -23,10 +19,7 @@ class App extends Component {
         <SignInForm/>
         <CSSTransition classNames={"App__policy"} timeout={300} in={this.state.loaded} unmountOnExit>
           <div className="App__policy-div">
-            <a className="App__policy"
-               onMouseOver={() => this.setState({lock: lockAqua})}
-               onMouseLeave={() => this.setState({lock: lock})}
-            >
+            <a className="App__policy">
               <svg className="App__lock" xmlns="http://www.w3.org/2000/svg" width="11" height="12" viewBox="0 0 11 12">
                 <g fill="none" fillRule="evenodd">
                   <path d="M.5 5.5h10v6H.5zM8.5 3.5c0-1.933-1.067-3-3-3s-3 1.067-3 3v2h6v-2z"/>
--- src/SignInForm/SignInForm.js
+++ src/SignInForm/SignInForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { CSSTransition } from 'react-transition-group';
+import InputMask from 'react-text-mask';
 import './SignInForm.css';
 
 const regex = /[^0-9 ]+/g;
@@ -19,31 +20,24 @@ class SignInForm extends Component {
     this.setState({ loaded: true })
   }
   onType(event){
-    if (!event.target.value.match(regex)){
-      let input = event.target.value;
-      if (this.state.input < input){
-        if (input.length === 2) input = input.slice(0,1) + ' ' + input.slice(1,2);
-        if (input.length === 6) input = input.slice(0,5) + ' ' + input.slice(5,6);
-      }
-      if (this.state.input > input){
-        if (input.length === 2) input = input.slice(0,1);
-        if (input.length === 6) input = input.slice(0,5);
-      }
-      if (input.length === 10) {
-        this.setState({input: input, buttonDisabled: false});
+    console.log(event.target.value);
+    if (event.target.value.length === 10) {
+      if(this.state.input.length < 10) {
         document.getElementById("dotGo1").beginElementAt(0.2);
         document.getElementById("dotGo2").beginElementAt(0.1);
         document.getElementById("dotGo3").beginElementAt(0);
         document.getElementById("enabledArrow").beginElementAt(0.2);
-      } else {
+      }
+      this.setState({input: event.target.value, buttonDisabled: false});
+    }
+    else {
         if(!this.state.buttonDisabled){
           document.getElementById("dotBack1").beginElementAt(0);
           document.getElementById("dotBack2").beginElementAt(0.1);
           document.getElementById("dotBack3").beginElementAt(0.2);
           document.getElementById("backArrow").beginElementAt(0);
         }
-        this.setState({input: input, buttonDisabled: true});
-      }
+        this.setState({input: event.target.value, buttonDisabled: true});
     }
   }
   onClickSelector(event){
@@ -91,15 +85,13 @@ class SignInForm extends Component {
               >
                 <a>+371 <i className="fa fa-angle-down"/></a>
               </button>
-              <input
-                type="text"
-                autoComplete={'off'}
-                id={'input'}
-                className="SignInForm__input"
-                autoFocus={true}
-                value={this.state.input}
-                maxLength={10}
-                onChange={(event) => { this.onType(event) }}/>
+              <InputMask id={'input'}
+                         className="SignInForm__input"
+                         autoFocus={true}
+                         value={this.state.input}
+                         mask={[/\d/,' ', /\d/,/\d/,/\d/, ' ', /\d/,/\d/,/\d/,/\d/]}
+                         guide={false}
+                         onChange={(event) => { this.onType(event) }}/>
             </form>
             {!this.state.clicked ? <button
               className="SignInForm__button"
